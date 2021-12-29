6073f5b43c1cc1b12ad988f2ca85a3d4e8a00138
Erik Sambell <erik@sambell.nl>
2021-12-29 13:16:31 +0100

    implemented get and post

--- strapi-fe/pages/api/request.tsx
+++ strapi-fe/pages/api/request.tsx
@@ -1,13 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
+
+interface Attributes {
+  createdAt: string;
+  publishedAt: string;
+  updatedAt: string;
+}
+
+export interface RequestAttributes {
+  name: string;
+  body: string;
+}
+
+interface AllAttributes extends Attributes, RequestAttributes {}
 export interface APIResponse {
   data: {
     id: number;
-    attributes: {
-      name: string;
-      body: string;
-      createdAt: string;
-      publishedAt: string;
-      updatedAt: string;
-    }[];
+    attributes: AllAttributes[];
   };
   meta: {
     pagination: {
@@ -24,4 +32,25 @@ export async function getContent(collection: string): Promise<APIResponse> {
   const response = await fetch(url);
   const json = await response.json();
   return json;
+}
+
+export async function postContent(
+  collection: string,
+  data: RequestAttributes
+): Promise<AxiosResponse> {
+  const url = `http://localhost:1337/api/${collection}s`;
+  const response = await authenticateUser().then((token: string) =>
+    axios.post(url, { data }, { headers: { Authorization: `Bearer ${token}` } })
+  );
+  return response;
+}
+
+/** returns jwt token */
+export async function authenticateUser(): Promise<string> {
+  const url = 'http://localhost:1337/api/auth/local';
+  const data = {
+    identifier: 'erik',
+    password: 'erikerik',
+  };
+  return axios.post(url, data).then((resp) => resp.data.jwt);
 }
--- strapi-fe/pages/components/Form.tsx
+++ strapi-fe/pages/components/Form.tsx
@@ -1,20 +1,28 @@
-import { FunctionComponent, SyntheticEvent } from 'react';
-import { APIResponse, getContent } from '../api/request';
+import { FunctionComponent, SyntheticEvent, Fragment } from 'react';
+import { APIResponse, getContent, postContent, authenticateUser } from '../api/request';
 
 const Form: FunctionComponent = () => {
   const submitHandler = (event: SyntheticEvent): void => {
     event.preventDefault();
-    console.log('submit!');
+    const name = event.target[0].value;
+    const body = event.target[1].value;
+    postContent('request', { name, body }).then((response) => console.log(response));
   };
 
-  getContent('request').then((response: APIResponse) => console.log(response.data));
+  const logRequests = (event: SyntheticEvent): void => {
+    getContent('request').then((response) => console.log(response));
+  };
 
   return (
-    <form onSubmit={submitHandler}>
-      <p>An input</p>
-      <input type="text" />
-      <button type="submit">Submit</button>
-    </form>
+    <Fragment>
+      <form onSubmit={submitHandler}>
+        <p>An input</p>
+        <input type="text" name="name" id="name" placeholder="Name" />
+        <input type="text" name="body" id="body" placeholder="Body" />
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={logRequests}>get requests</button>
+    </Fragment>
   );
 };
 
