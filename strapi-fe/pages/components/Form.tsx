16995b630c778ea80331f54149448468bab39542
Erik Sambell <erik@sambell.nl>
2021-12-29 13:49:07 +0100

    using refs and added typing

--- strapi-fe/pages/components/Form.tsx
+++ strapi-fe/pages/components/Form.tsx
@@ -1,15 +1,25 @@
-import { FunctionComponent, SyntheticEvent, Fragment } from 'react';
-import { APIResponse, getContent, postContent, authenticateUser } from '../api/request';
+import { FunctionComponent, SyntheticEvent, Fragment, useRef, RefObject } from 'react';
+import { getContent, postContent } from '../api/request';
 
 const Form: FunctionComponent = () => {
+  const nameRef = useRef<HTMLInputElement>(new HTMLInputElement());
+  const bodyRef = useRef<HTMLInputElement>(new HTMLInputElement());
+
   const submitHandler = (event: SyntheticEvent): void => {
     event.preventDefault();
-    const name = event.target[0].value;
-    const body = event.target[1].value;
-    postContent('request', { name, body }).then((response) => console.log(response));
+
+    if (isValidRef(nameRef) && isValidRef(bodyRef)) {
+      const name: string = nameRef.current.value;
+      const body: string = bodyRef.current.value;
+      postContent('request', { name, body }).then((response) => console.log(response.data.data));
+    }
   };
 
+  const isValidRef = (ref: RefObject<HTMLInputElement>): boolean =>
+    !!ref?.current?.value && ref.current.value !== '';
+
   const logRequests = (event: SyntheticEvent): void => {
+    event.preventDefault();
     getContent('request').then((response) => console.log(response));
   };
 
@@ -17,8 +27,8 @@ const Form: FunctionComponent = () => {
     <Fragment>
       <form onSubmit={submitHandler}>
         <p>An input</p>
-        <input type="text" name="name" id="name" placeholder="Name" />
-        <input type="text" name="body" id="body" placeholder="Body" />
+        <input type="text" name="name" id="name" placeholder="Name" ref={nameRef} />
+        <input type="text" name="body" id="body" placeholder="Body" ref={bodyRef} />
         <button type="submit">Submit</button>
       </form>
       <button onClick={logRequests}>get requests</button>
